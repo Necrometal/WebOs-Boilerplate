1ef0bdbcf483131e36f7e09f9006b6d983ed7c00
Michel-Hairun <michel.rak@hairun-technology.com>
2023-06-16 16:14:37 +0300

    add smooth scroll to spotable component navigation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,12 +3,20 @@ import Panels from '@enact/sandstone/Panels';
 import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
 import { RouterProvider } from 'react-router-dom';
 import Router from '../Router';
+import { FocusEvent } from "react";
+
+const scrollTo = (e: FocusEvent<HTMLElement, Element>) => {
+  e.target.scrollIntoView({ behavior: 'smooth', block: 'center' })
+}
 
 const App = kind({
 	name: 'App',
 
 	render: (props) => (
-		<Panels {...props}>
+		<Panels 
+      {...props} 
+      onFocus={scrollTo} 
+    >
       <RouterProvider router={Router} {...props}/>
     </Panels>
 	)
