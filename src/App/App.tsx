b75ef8f0e282c4e7e3b3ca15b54e93f5fc9900e7
Michel-Hairun <michel.rak@hairun-technology.com>
2023-06-22 17:23:24 +0300

    set Luna context provider on root app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import Router from '../Router';
+import LunaContext from '../Context/L2SRequest';
+import LS2Request from '@enact/webos/LS2Request';
 
+const LunaService = new LS2Request()
 
-const App = () => <RouterProvider router={Router}/>;
+const App = () => (
+  <LunaContext.Provider value={LunaService}>
+    <RouterProvider router={Router}/>
+  </LunaContext.Provider>
+)
 
 export default (App);
